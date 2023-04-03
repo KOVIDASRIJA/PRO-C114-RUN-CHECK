841f27ee5bdcf656f91d9fffb340313e1c3bde63
KOVIDASRIJA <99341305+KOVIDASRIJA@users.noreply.github.com>
2023-04-03 07:29:31 -0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+a = int(input("Enter the number a:- "))
+b = int(input("Enter the number b:- "))
+
+#a = 33
+#b = 4
+
+if b > a :
+    print(" b is graeter than a")
+
+elif a > b :
+    print("a is graeter than b")
+
+elif a == b :
+    print("'a' and 'b' is equal")
